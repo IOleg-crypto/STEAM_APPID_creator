decfe247c6d901b5d271e4ea9841aa64e25b7b83
IOleg-sus <guchkooleg7@gmail.com>
2023-11-12 11:11:33 +0200

    recoding some stuff

--- 1.0_steam_appid.cpp
+++ 1.0_steam_appid.cpp
@@ -2,34 +2,33 @@
 #include <string>
 #include <string.h>
 #include <fstream>
-#define DEBUG_MESSAGE_APPEAR std::cout << "The steam appid is created!!!!!";
-#define PASS std::cout << "\n";
+#include "info_header.h"
+ 
 int main()
 {
-    std::cout << "HELLO , THAT IS PROGRAM WHICH CREATING FOR GAME STEAM_APPID IF STEAM DELETING(FOR NON STEAM GAME)" << std::endl;
-    PASS;
+    
     std::fstream fill;
-    int appid = 0;
-    std::string path = "";
+    int GetAPPID = 0;
+    std::string GETINFO = "";
+    INFO_ABOUT_PATH;
+    START;
     PASS;
-    std::cout << "Write the appid for game(if you know) : ";
-    std::cin >> appid;
-    std::cout << "For example :  D:\\SteamLibrary\\steamapps\\common\\Half-Life\\bavl\\Half-Life\\steam_appid.txt(name of file needs at the end , cause it never work)";
+    EXAMPLE_INFO;
     PASS;
-    std::cout << "Path to folder -> where you need create this file :  ";
-    std::cin >> path;
-    fill.open(path, std::ios::out);
-    DEBUG_MESSAGE_APPEAR;
+    INFO_ABOUT_PATH_2;
+    // std::string paths = "";
+    std::cin >> GETINFO;
+    INFO_ABOUT_PATH_3;
+    std::cin >> GetAPPID;
+    // doinfo.setInfo(paths, appid);
+    // doinfo.CreateFile()
+
+    fill.open(GETINFO, std::ios::out);
     if (fill.is_open())
     {
-        fill << appid;
+        fill << GetAPPID;
+        DEBUG_MESSAGE_APPEAR;
         fill.close();
     }
-    /*
-    else
-    {
-        throw "Error";
-    }
-    */
-    std::system("pause");
+    std::system("pause"); // lock fast close of program
 }
---
+++ info_header.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef _INFO_HEADER_H
+#define _INFO_HEADER_H
+#define START std::cout << "-------------------------------------------------------------------------------------------------------------------";
+#define DEBUG_MESSAGE_APPEAR std::cout << "The steam appid is created!!!!!";
+#define PASS std::cout << "\n";
+#define INFO_ABOUT_PATH std::cout << "Hello , THAT IS PROGRAM WHICH CREATING FOR GAME STEAM_APPID IF STEAM DELETING(FOR NON STEAM GAME)" << std::endl;
+#define INFO_ABOUT_PATH_2 std::cout << "Path to folder -> where you need create this file :  ";
+#define INFO_ABOUT_PATH_3 std::cout << "Write the appid for game(if you know) : ";
+#define EXAMPLE_INFO std::cout << "For example :  D:\\SteamLibrary\\steamapps\\common\\Half-Life\\bavl\\Half-Life\\steam_appid.txt(name of file needs at the end , cause it never work)";
+#endif
